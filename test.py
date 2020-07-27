5fbc27654ffe56eaf67423d0ac32e7c34e2815f4
xfx <1064222854@qq.com>
2020-07-27 21:55:03 +0800

    修改一个分支

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-def reverseString(s):
-    """
-    :type s: List[str]
-    :rtype: None Do not return anything, modify s in-place instead.
-    """
-    left = 0
-
-    right =len(s)-1
-
-
-    
-
-
-s=["h","e","l","l","o"]
-reverseString(s)
-print(s)
+print('xiugaiyixia ')
